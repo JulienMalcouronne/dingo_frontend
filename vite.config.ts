b0ed20d8a23778f03ffedabbbc0fd7626deb63b2
julien malcouronne <malcouronnejulien@gmail.com>
2024-08-24 18:21:14 +0200

    feat: init front repo + setup backend con

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [sveltekit()],
+  server: {
+    proxy: {
+      "/api": import.meta.env.VITE_API_URL,
+    },
+  },
+});
